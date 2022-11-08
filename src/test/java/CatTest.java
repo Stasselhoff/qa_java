b9105fc490d306e76f80cd40a4d59f07d4d2ca83
S.benevolenskaya <s.benevolenskaya@securitycode.ru>
2022-11-09 01:55:50 +0300

    add jacoco report

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class testCat {
+
+    @Mock
+    private Feline feline;
+
+    @Test
+    public void getFood() throws Exception {
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        assertEquals(expected, cat.getFood());
+    }
+
+    @Test
+    public void getSound() {
+        String myau = "Мяу";
+        assertEquals("Ошибка теста мяу", myau, "Мяу");
+    }
+}
